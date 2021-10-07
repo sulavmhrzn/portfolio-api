3049dce5be8d14ed48af9b7ed3ac572afcc2c166
sulav mhrzn <sulavmaharjan63@gmail.com>
2021-10-07 08:56:53 +0545

    Use environment variables for admin panel url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,25 @@
-from django.contrib import admin
-from django.urls import path, include
+import os
+
+import decouple
 from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+
+def get_admin_panel_url() -> str:
+    """assign ADMIN_PANEL_URL envvar to admin_panel variable."""
+    try:
+        admin_panel_url = decouple.config("ADMIN_PANEL_URL")
+    except decouple.UndefinedValueError:
+        try:
+            admin_panel_url = os.environ["ADMIN_PANEL_URLs"]
+        except KeyError:
+            raise Exception("ADMIN_PANEL_URL was not set as environment variable.")
+    return admin_panel_url
+
 
 urlpatterns = [
-    path("hiddenadminpanel/", admin.site.urls),
+    path(get_admin_panel_url(), admin.site.urls),
     path("", include("portfolio.urls")),
 ]
 
